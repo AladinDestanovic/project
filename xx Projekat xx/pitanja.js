d0b691850d0beb8983dd304f4b8044ea497bdd67
AladinDestanovic <111779715+AladinDestanovic@users.noreply.github.com>
2023-03-08 11:16:20 -0800

    Update pitanja.js

--- xx Projekat xx/pitanja.js
+++ xx Projekat xx/pitanja.js
@@ -91,4 +91,4 @@ document.getElementById("c").addEventListener("click", () => {
   submitAnswer(questionn.id, ans);
 });
 let total = 30;
-window.location.href = "succes.html?points=" + points;
+window.location.href = "result.html?points=" + points;
