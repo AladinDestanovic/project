6ddaacf1d155be5253ef018981094dd8c3cfd140
AladinDestanovic <111779715+AladinDestanovic@users.noreply.github.com>
2023-03-08 19:46:54 +0100

    Add files via upload

--- xx Projekat xx/pitanja.js
+++ xx Projekat xx/pitanja.js
@@ -1,59 +1,3 @@
-// let bodovi = 0;
-// document.getElementById("ADiv").addEventListener("click", function () {
-//   document.getElementById("ADiv").style.backgroundColor = "green";
-//   if (bodovi < 1) {
-//     bodovi = bodovi + 10;
-//     console.log(bodovi);
-//   } else {
-//   }
-// });
-// document.getElementById("BDiv").addEventListener("click", function () {
-//   document.getElementById("ADiv").style.backgroundColor = "green";
-//   document.getElementById("BDiv").style.backgroundColor = "red";
-
-//   if (bodovi < 1) {
-//     bodovi = bodovi + 5;
-//     console.log(bodovi);
-//   } else {
-//   }
-// });
-// document.getElementById("CDiv").addEventListener("click", function () {
-//   document.getElementById("ADiv").style.backgroundColor = "green";
-//   document.getElementById("CDiv").style.backgroundColor = "red";
-
-//   if (bodovi < 1) {
-//     bodovi = bodovi + 5;
-//     console.log(bodovi);
-//   } else {
-//   }
-// });
-// document.getElementById("DDiv").addEventListener("click", function () {
-//   document.getElementById("ADiv").style.backgroundColor = "green";
-//   document.getElementById("DDiv").style.backgroundColor = "red";
-
-//   if (bodovi < 1) {
-//     bodovi = bodovi + 5;
-//     console.log(bodovi);
-//   } else {
-//   }
-// });
-// document.getElementById("seeResults").addEventListener("click", () => {
-//   if (bodovi < 1) {
-//     alert("niste odgovorili na dato pitanje");
-//   } else {
-//     window.location.href = "results.html";
-//   }
-// });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const showButton = document.getElementById("show");
-//   const pointsElement = document.getElementById("points");
-
-//   showButton.addEventListener("click", () => {
-//     pointsElement.textContent = bodovi + "/10";
-//   });
-// });
-
 let points = 0;
 const arr = [
   {
@@ -89,22 +33,6 @@ const startTest = () => {
 };
 startTest();
 
-///
-// const click = () => {
-//   let i = 0;
-//   document.getElementById("a").addEventListener("click", () => {
-//     submitAnswer(arr[0].id, arr[0].answers[0]);
-//     question = arr[0];
-//   });
-//   document.getElementById("b").addEventListener("click", () => {
-//     submitAnswer(arr[0].id, arr[0].answers[1]);
-//     question = arr[0];
-//   });
-//   document.getElementById("b").addEventListener("click", () => {
-//     submitAnswer(arr[0].id, arr[0].answers[2]);
-//     question = arr[0];
-//   });
-// };
 let i = 0;
 
 questionn = arr[i];
@@ -162,24 +90,4 @@ document.getElementById("c").addEventListener("click", () => {
   const ans = document.getElementById("c").textContent;
   submitAnswer(questionn.id, ans);
 });
-// };
-// startTest();
-
-// const submitAnswer = (id, ans) => {
-// 1. Uporedi da li je id za koji smo submitovali odgovor i id trenutnog pitanja isti
-// question.id === id
-
-// 2. Da li je nas "ans" jednak "correct"
-
-// 3. Ako jeste dodaj bodove
-
-// 4. Ako postoji sledece pitanje prikazi ga i sacuvaj to pitanje u varijabli "question"
-
-// 5. Ako ne postoji novo pitanje, onda me odvedi na success page
-// }+
-
-// document.getElementById("ans4").addEventListener("click", () => {
-//   const ans = document.getElementById("ans4").textContent;
-//   submitAnswer(question.id, ans);
-// });
 let total = 30;
